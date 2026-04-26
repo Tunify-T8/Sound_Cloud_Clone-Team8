cc5054be432136248ab630885099f034164e13f1
Amir Diaa <amirdiaa72005@gmail.com>
2026-04-26 17:04:08 +0300

    Refactor AppService to implement OnModuleInit and add logging on startup

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+  private readonly logger = new Logger(AppService.name);
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  onModuleInit(): void {
+    this.logger.log('Hello World from backend startup');
   }
 }
